ca7252464d39cfbfc6db3359d8d6282b009fb08c
Chris Chen <chrischen0618@gmail.com>
2020-03-21 11:17:13 +0800

    Add network related scripts
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- scan_network.sh
+++ scan_network.sh
@@ -2,10 +2,16 @@
 ip_range="192.168.1"
 
 function ping-scan() {
+    if [ $# -ne 0 ]; then
+        ip_prefix="$1"
+    else
+        ip_prefix=ip_range
+    fi
+
     for ip_seg in $(
         for ip in `seq 1 255`; do
-            ping -c 1 "${ip_range}.${ip}" 1>/dev/null 2>/dev/null && echo ${ip} &
+            ping -c 1 "${ip_prefix}.${ip}" 1>/dev/null 2>/dev/null && echo ${ip} &
         done 2>/dev/null | sort -g); do
-        echo "${ip_range}.${ip_seg}";
+        echo "${ip_prefix}.${ip_seg}";
     done
 }
