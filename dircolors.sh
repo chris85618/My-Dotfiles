cf661f646055b20421f26d52fb11a4bafe403613
Chris Chen <chris.chen@insyde.com>
2020-06-12 15:42:47 +0800

    Adjust export.sh
    Signed-off-by: Chris Chen <chris.chen@insyde.com>

---
+++ dircolors.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# here's LS_COLORS
+# github.com/trapd00r/LS_COLORS
+eval "$(dircolors -b ~/.setup/dircolors)"
+
--- exports.sh
+++ exports.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 export PATH="${HOME}/.local/bin:$PATH"
-
-# Don’t clear the screen after quitting a manual page
-export MANPAGER="less -X"
-
-# here's LS_COLORS
-# github.com/trapd00r/LS_COLORS
-eval "$(dircolors -b ~/.setup/dircolors)"
