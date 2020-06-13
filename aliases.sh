a4b6c50f4d937452b3fab8ef5b930e0cbf947be6
Chris Chen <chrischen0618@gmail.com>
2020-06-13 11:55:22 +0800

    Separate the alias scripts
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- aliases.sh
+++ aliases.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-
-if [ -f /usr/share/bash-completion/bash_completion ]; then
-    source /usr/share/bash-completion/bash_completion
-elif [ -f /etc/bash_completion ]; then
-    source /etc/bash_completion
-fi
-
-function new_alias() {
-    alias_name=$1
-    real_command=$2
-    src_cmdname=${real_command}
-
-    alias "${alias_name}"="${real_command}"
-
-    # Load all the complete commands
-    _completion_loader "${src_cmdname}" 1>/dev/null 2>/dev/null
-    # Check those commands can be auto completed
-    complete_cmd="$(complete -p ${src_cmdname} 2>/dev/null)"
-    if [ $? -eq 0 ] ; then
-        # Complete them
-        $(echo "${complete_cmd}" | sed "s/${src_cmdname}[ \t\r]*$/${alias_name}/g")
-    fi
-}
-
 function keep-try() {
     counter=1
     while : ; do
