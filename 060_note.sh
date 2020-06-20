7c44ac666736864a23c2a45d98be3de391f033ef
Chris Chen <chrischen0618@gmail.com>
2020-06-21 07:15:07 +0800

    Improve the mechanism to fetch IP Add alias 'get_ip' to achieve the goal
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- 060_note.sh
+++ 060_note.sh
@@ -7,8 +7,15 @@ fi
 
 function note() {
     if [ $# -eq 1 ]; then
-        update_ip ${1}
+        # Store the origin variable 'iprange'
+        ori_iprange=${iprange}
+        # export the iprange for 'note'
+        export iprange=${1}
+        # Get the actually IP
+        my_ip=$(get_ip)
+        # restore
+        export iprange=${ori_iprange}
     fi
-    cat ${NOTE} | sed -e "s/\${RAND_MAC}/$(rand-mac)/g" -e "s:\${mine}:${my_ip}:g"
+    cat ${NOTE} | sed -e "s/\${RAND_MAC}/$(rand-mac)/g" -e "s:\${MINE}:${my_ip}:g"
 }
 alias editnote="${EDITOR} ${NOTE}"
