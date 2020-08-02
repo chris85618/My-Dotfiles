f127cfc385a1a78fb93cc4dac66122ddae0219c9
Chris Chen <chrischen0618@gmail.com>
2020-08-02 15:21:24 +0800

    Reformatter chrome_robot.py
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- chrome_robot.py
+++ chrome_robot.py
@@ -36,31 +36,31 @@ class MySelenium(RemoteWebDriver.WebDriver):
             height=self.get_window_size()['height'])
 
         time.sleep(0.3)
-        window_height=self.execute_script(
+        window_height = self.execute_script(
             "return window.innerHeight")
 
-        result=Image.new('RGB', (total_width, total_height))
+        result = Image.new('RGB', (total_width, total_height))
 
-        times=int(total_height / window_height)
+        times = int(total_height / window_height)
         for i in range(times):
             self.execute_script(
                 "window.scrollTo(0, {});".format(window_height * i))
             time.sleep(0.2)
-            base64_image=self.get_screenshot_as_base64()
-            base64_image_io=io.BytesIO(base64.b64decode(base64_image))
-            image=Image.open(base64_image_io)
+            base64_image = self.get_screenshot_as_base64()
+            base64_image_io = io.BytesIO(base64.b64decode(base64_image))
+            image = Image.open(base64_image_io)
             result.paste(image, (0, window_height * i))
             image.close()
 
         # Last One
-        last_height=total_height % window_height
+        last_height = total_height % window_height
         if last_height > 0:
             self.execute_script("window.scrollTo(0, {});".format(total_height))
             time.sleep(0.2)
-            base64_image=self.get_screenshot_as_base64()
-            base64_image_io=io.BytesIO(base64.b64decode(base64_image))
-            image=Image.open(base64_image_io)
-            crop_image=image.crop(
+            base64_image = self.get_screenshot_as_base64()
+            base64_image_io = io.BytesIO(base64.b64decode(base64_image))
+            image = Image.open(base64_image_io)
+            crop_image = image.crop(
                 (0, image.size[1] - last_height, image.size[0], image.size[1]))
             result.paste(crop_image, (0, total_height - last_height))
             image.close()
@@ -69,32 +69,32 @@ class MySelenium(RemoteWebDriver.WebDriver):
             result.save(target_file)
 
 
-RemoteWebDriver.WebDriver=MySelenium
+RemoteWebDriver.WebDriver = MySelenium
 
 
 class Chromedriver(webdriver.Chrome):
-    def __init__(self, *args, chrome_options = Options(), **kwargs):
+    def __init__(self, *args, chrome_options=Options(), **kwargs):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--disable-infobars')
-        super().__init__(*args, chrome_options = chrome_options, **kwargs)
+        super().__init__(*args, chrome_options=chrome_options, **kwargs)
         atexit.register(self.quit)
 
-    def screenshot_all(self, filename = DEFAULT_SCREENSHOT_TARGET):
+    def screenshot_all(self, filename=DEFAULT_SCREENSHOT_TARGET):
         MySelenium.screenshot_all(self, filename)
 
 
 def open_webs(*sites):
-    browser=Chromedriver()
+    browser = Chromedriver()
 
-    handle=browser.window_handles[0]
+    handle = browser.window_handles[0]
 
-    is_first_tab=True
+    is_first_tab = True
 
     for link in sites:
         if is_first_tab:
-            is_first_tab=False
+            is_first_tab = False
             browser.get(link)
         else:
             browser.execute_script("window.open('{url}');".format(url=link))
@@ -109,8 +109,8 @@ def open_webs(*sites):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        sites=sys.argv[1:]
+        sites = sys.argv[1:]
     else:
-        sites=[]
+        sites = []
 
-    browser=open_webs(*sites)
+    browser = open_webs(*sites)
