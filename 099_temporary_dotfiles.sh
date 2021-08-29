147be59f37df8632f6cddaaf4bd2fb2ff788c11a
Chris Chen <chrischen0618@gmail.com>
2021-08-29 18:01:06 +0800

    Fix that it couldn't find ".setup/temporary_dotfiles/*.sh" when opening     the terminal if there wasn't any temporary dotfile
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- 099_temporary_dotfiles.sh
+++ 099_temporary_dotfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for script in ${HOME}/.setup/temporary_dotfiles/*.sh; do
-    . ${script}
-done
+if [ $(ls -1 ${HOME}/.setup/temporary_dotfiles/*.sh 2>/dev/null | wc -l) -ne 0 ]; then
+    for script in ${HOME}/.setup/temporary_dotfiles/*.sh; do
+       . ${script}
+    done
+fi
