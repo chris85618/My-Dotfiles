b72f004da5701abb58a58e48dad0e498878c59c5
Chris Chen <chrischen0618@gmail.com>
2022-11-11 22:03:06 +0800

    Remove useless error message to lookup ${HOME}/*
    Signed-off-by: Chris Chen <chrischen0618@gmail.com>

--- 030_home.sh
+++ 030_home.sh
@@ -4,7 +4,7 @@ function copy-from() {
     cp -rfv ${@:2} ${1}
 }
 
-for p in $(find $HOME/* -maxdepth 0 -mindepth 0 -type d); do
+for p in $(find $HOME/* -maxdepth 0 -mindepth 0 -type d 2>/dev/null); do
     dirname=$(basename ${p})
     alias c${dirname}="cd ${p}"
     alias to${dirname}="copy-from ${p} "
