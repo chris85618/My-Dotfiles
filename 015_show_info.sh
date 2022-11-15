6dbcae3a5c5997ee0ad0a522f3b92ffc3cbd99fd
Chris Chen <chrischen0618@gmail.com>
2022-11-15 10:58:49 +0800

    Fix that '${COLUMN}' undefined

--- 015_show_info.sh
+++ 015_show_info.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hostname | xargs -d. | awk '{print $1}' | grep -v '^$' | figlet -w ${COLUMNS}
+hostname | xargs -d. | awk '{print $1}' | grep -v '^$' | figlet -w $(tput cols)
 echo "Hello, ${USER}"
