43d74798cf7ded5d795b20e78c1ffeee0e49d6a6
陳謙 <t112598078@ntut.edu.tw>
2024-04-17 14:36:17 +0800

    Add script to export the environment variables of Android SDK
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

---
+++ 021_exports_android_sdk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -d $HOME/Android/Sdk ]; then
+    export ANDROID_HOME=$HOME/Android/Sdk
+    export PATH=$PATH:$ANDROID_HOME/emulator
+    export PATH=$PATH:$ANDROID_HOME/platform-tools
+fi
